11a2098577191893c46ee0be4415271de17728a2
rikodwi <90703057+rikodwi@users.noreply.github.com>
2023-01-02 01:20:06 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = 18155804
+API_HASH = "e076156e161d0443b49382adb99595af"
+BOT_TOKEN = "5962175498:AAFT8_f7rKAA1_T8VDG4qdkezvp-6iMWKTg"
+SESSION = "BQAbYFG2F2AL-sFesfQurpDin_-6ni9BdlqqQcRVGRu1qeTZr0Pa7Oz6-_2ioGf_VCIapeOzc8BtU_PV5l0iWoQezzfWG52JOTyDxvhzYr74ArPQYzogcH416YFDDfw-qpEFVtokGZ8pLPVZ6EXsMvNhK4jsYRynqpMOE9B1PzlgEbvDwFESqG0FSV7MgF5TpruYfCHrTl1wkUT8bEDT8LqfPBpYoGfKSXlqL-YPkR3YmiUn4LZq1MLTKC37BFmV6R7yPl1nOrK0ibO_N0ylSmd4fNhHvdYyEaqQYmrmjg9394AoTMetF-DRznjZWVMmcQMxXVdutpIUPdWIG3FPOv_kbXwuvgA"
+FORCESUB = "mabar_mobile_legends_bang_bang"
+AUTH = "1316840924"
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
